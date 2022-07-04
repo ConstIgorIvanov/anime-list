57c0c56c735bc207b5cebb226f1e0f0960f5680c
Ivanov I.A <constigorivanov@gmail.com>
2022-07-04 21:55:33 +0500

    loginpage layout + adaptiv

--- src/pages/LoginPage/index.tsx
+++ src/pages/LoginPage/index.tsx
@@ -1,7 +1,21 @@
 import React from 'react';
 
 const LoginPage = () => {
-  return <div>LoginPage</div>;
+  return (
+    <div className="login">
+      <div className="login__left">
+        <div className="login__left__title">A new level of anime listing</div>
+        <div className="login__left__description">
+          Lorem ipsum dolor sit amet, consectetur adipLorem ipsum dolor sit amet, consectetur adip
+          Lorem ipsum dolor sit amet, consectetur adip Lorem ipsum dolor sit amet, consectetur adip
+        </div>
+        <button className="button">Create a list</button>
+      </div>
+      <div className="login__right">
+        <div className="login__right__image"></div>
+      </div>
+    </div>
+  );
 };
 
 export default LoginPage;
